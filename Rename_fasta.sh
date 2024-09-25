d7bf56dbced476e6e4c6d985ffbb347dcef3e455
wzuhou <wuz1992@hotmail.com>
2024-09-25 14:23:04 +0100

    Update Rename_fasta.sh

--- Rename_fasta.sh
+++ Rename_fasta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./WCS/comparative_genome/Fasta
+cd ./comparative_genome/Fasta
 
 cat Z_capensis.fasta | seqkit replace --ignore-case --keep-key --line-width 0 --kv-file < (cat Z_capensis.fasta.fai.rename) --pattern "(.+)" --replacement "{kv}" > Z_capensis_rename.fa
 
